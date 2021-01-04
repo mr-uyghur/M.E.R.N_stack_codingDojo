cb4611ab57e74da84e917fffbc902809ab03dd05
alimran halmamat <mr.uygur1996@gmail.com>
2021-01-04 15:33:03 -0800

    first JSX

---
+++ React/my-first-project/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import React from 'react'
+// importing component from components folder
+import NewComponent from './components/newComponent'
+
+function App() {
+  return (
+    <div className="App">
+   <NewComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/my-first-project/src/components/newComponent.js
@@ -0,0 +1,21 @@
+
+
+import React, { Component } from 'react';
+    
+    
+class NewComponent extends Component {
+    render() {
+        return <div>
+            <h1>Hello Dojo</h1>
+            <h2>Things I need to do </h2>
+            <ul>
+                <li> Learn React</li>
+                <li>Climb Mt. Everest</li>
+                <li> Run a marathon</li>
+                <li> Feed the Dogs</li>
+                </ul>
+        </div>;
+    }
+}
+    
+export default NewComponent;
