c7e225db71d96bd5b1e63198d618145338822c5a
alimran halmamat <mr.uygur1996@gmail.com>
2021-01-07 12:37:03 -0800

    W1D4 work

---
+++ React/chat-app-test/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './App.css';
+import MessageForm from './components/MessageForm';
+import MessageDisplay from './components/MessageDisplay';
+
+function App() {
+  const [currentMsg, setCurrentMsg] = useState("There are no messages");
+
+  const youveGotMail = ( newMessage ) => {
+    setCurrentMsg( newMessage );
+}
+
+  return (
+    <div className="App">
+  <MessageForm onNewMessage={ youveGotMail } />
+            <MessageDisplay message={ currentMsg } />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/chat-app-test/src/components/MessageForm.jsx
@@ -0,0 +1,30 @@
+import react, { useState } from 'react';
+    
+    
+const MessageForm = (props) => {
+    const [msg, setMsg] = useState("");
+
+
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.onNewMessage( msg );
+    };
+    
+    
+    return (
+        <form onSubmit={ handleSubmit }>
+            <h1>Send Message</h1>
+            <textarea 
+                rows="4"
+                cols="50"
+                placeholder="Enter your message here"
+                onChange={ (e) => setMsg(e.target.value) }
+                value={ msg }
+            ></textarea>
+            <input type="submit" value="Send Message" />
+        </form>
+    );
+};
+    
+export default MessageForm;
