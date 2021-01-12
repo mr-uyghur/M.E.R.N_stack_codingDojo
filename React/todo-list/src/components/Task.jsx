8ab6fca40a35c73f14ee64395328519baec2e2c7
alimran halmamat <mr.uygur1996@gmail.com>
2021-01-11 19:23:32 -0800

    perfected todo-list

--- React/todo-list/src/components/Task.jsx
+++ React/todo-list/src/components/Task.jsx
@@ -11,20 +11,25 @@ const Task = (props) => {
     const onChange = () => {
         list[index].isComplete = !list[index].isComplete
         setList([...list])
-        if(task.isComplete == true){
-            console.log('its true')
-            list[index].name = "task completed!"
-        }
     }
+
+    const checked = {
+        'text-decoration' : 'line-through'
+    }
+
+    const unChecked = {}
 
     return (
         <div className="p-3 mb-2 bg-light text-dark">
-            <p> {task.name}</p>
+            <p style = {task.isComplete ? checked : unChecked}> {task.name}</p>
             <label htmlFor='checkbox'> Completed task? </label>
-            <input onChange={onChange} type='checkbox' name='' checked={task.isComplete} />
+         
+           <input   onChange={onChange} type='checkbox' name='' checked={task.isComplete} />
+            
             <button className="btn btn-danger" onClick={onClick}>Delete</button>
         </div>
     )
+
 }
 
 export default Task
